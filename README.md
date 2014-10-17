3f0296900d9bd8178e92c08e0a87ffffd90e0426
Mihai Vintila <mihaiv@intern.pch>
2014-10-17 16:23:09 +0300

    prevent the script for running on other version; multiple improvements

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-## VoipNow SPE Patches Repository
+## VoipNow Patches Repository
 
-This repository contains a list of quick-fix patches that can be applied to VoipNow SPE. 
+This repository contains a list of quick-fix patches that can be applied to VoipNow. 
 
 The following table contains a description of the available patches:
 
 |File Name|Date|Description|
 |---------|----|-----------|
-|[sslpoodlefix.sh](sslpoodlefix.sh?raw=true)|2014.10.16|Fix SSLv3 vulnerability (CVE-2014-3566) in VoipNow Professional 2.x and VoipNow SPE 3.0.0-3.0.5|
+|[sslpoodlefix.sh](sslpoodlefix.sh?raw=true)|2014.10.16|Fix SSLv3 vulnerability (CVE-2014-3566) in VoipNow  2.x.x - 3.0.5|
--- sslpoodlefix.sh
+++ sslpoodlefix.sh
@@ -3,11 +3,11 @@
 # Copyright (c) 2002-2014 by 4PSA (www.4psa.com)
 
 #Description 
-#The following script fixes patches VoipNow Professional 2.x and VoipNow SPE 3.0.x against CVE-2014-356
+#The following script patches VoipNow  2.x - 3.0.5 against CVE-2014-356
 check_voipnow()
 {
         if [ -f /usr/local/voipnow/.version ];then
-                version=`/bin/awk '{print $1}' /usr/local/voipnow/.version|awk -F'.' '{print $1}'`
+                version=`/bin/awk '{print $1}' /usr/local/voipnow/.version|awk -F'.' '{print $1$2$3}'`
         else
                 echo " VoipNow does not seem to be installed on this system!!!"
                 exit 0
@@ -15,19 +15,25 @@ check_voipnow()
 }
 check_voipnow
 #backup config first
-if [ ${version} == "3" ];then
+if [ ${version} -ge 300 -a ${version} -le 305 ];then
         echo "==> Backup config to /usr/local/voipnow/admin/conf/voipnow.conf.sslfixbackup"
         /bin/cp -fp /usr/local/voipnow/admin/conf/voipnow.conf /usr/local/voipnow/admin/conf/voipnow.conf.sslfixbackup
         echo "==> Disable SSLv3 protocol"
-        /bin/sed -i 's/^[ \t]ssl_protocols.*$/\tssl_protocols TLSv1 TLSv1.1 TLSv1.2;/g' /usr/local/voipnow/admin/conf/voipnow.conf
+        /bin/sed -i 's/^.*ssl_protocols.*$/\tssl_protocols TLSv1 TLSv1.1 TLSv1.2;/g' /usr/local/voipnow/admin/conf/voipnow.conf
         echo "==> Restart Web Management server"
         /etc/init.d/httpsa restart
 fi
-if [ ${version} == "2" ];then
+if [ ${version} -ge 200 -a ${version} -lt 300 ];then
+        if [ `grep -Ec "^.*ssl\.use-sslv3.*disable.*$" /usr/local/voipnow/admin/conf/voipnow.conf` -ge 1 ];then
+            echo "==> Patch already aplied "
+            exit 0
+        fi
+                                               	
         echo "==> Backup config to /usr/local/voipnow/admin/conf/voipnow.conf.sslfixbackup"
         /bin/cp -fp /usr/local/voipnow/admin/conf/voipnow.conf /usr/local/voipnow/admin/conf/voipnow.conf.sslfixbackup
         echo "==> Disable SSLv3 protocol"
-        /bin/sed -i 's/^[ \t]*ssl.engine.*$/\tssl.engine                              = "enable"\n\tssl.cipher-list         = "ECDHE-RSA-AES256-SHA384:AES256-SHA256:RC4:HIGH:!MD5:!aNULL:!eNULL:!NULL:!DH:!EDH:!AESGCM"\nssl.use-sslv2           = "disable"\nssl.use-sslv3           = "disable"\n\tssl.use-compression     = "disable"\n\tssl.honor-cipher-order  = "enable"\n/' /usr/local/voipnow/admin/conf/voipnow.conf
+        /bin/sed -i 's/^.*ssl.cipher-list.*$//' /usr/local/voipnow/admin/conf/voipnow.conf
+        /bin/sed -i 's/^.*ssl.engine.*$/\tssl.engine\t= "enable"\n\tssl.cipher-list\t= "ECDHE-RSA-AES256-SHA384:AES256-SHA256:RC4:HIGH:!MD5:!aNULL:!eNULL:!NULL:!DH:!EDH:!AESGCM"\n\tssl.use-sslv2\t= "disable"\n\tssl.use-sslv3\t= "disable"\n\tssl.honor-cipher-order\t= "enable"\n/' /usr/local/voipnow/admin/conf/voipnow.conf
         echo "==> Restart Web Management Server"
         /etc/init.d/voipnow restart
 
